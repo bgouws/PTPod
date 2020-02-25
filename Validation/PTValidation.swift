c33be9de5888d9c97a6b47954aeb68fa7de958e2
Brandon Gouws <bgouws@jhb.dvt.co.za>
2020-02-25 14:32:02 +0200

    Test

--- Validation/PTValidation.swift
+++ Validation/PTValidation.swift
@@ -13,6 +13,7 @@ public class PTValidation {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)
+        //A Random Test
     }
 }
 
---
+++ test.swift
@@ -0,0 +1,9 @@
+//
+//  test.swift
+//  FirebaseAuth
+//
+//  Created by Brandon Gouws on 2020/02/25.
+//
+
+import Foundation
+//Some random data
