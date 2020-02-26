ea9dcf71a0e95d7e6407ae2304bf4875a5169608
Brandon Gouws <bgouws@jhb.dvt.co.za>
2020-02-26 06:38:18 +0200

    Added validation file

---
+++ PTFramework/Classes/ValidationVM.swift
@@ -0,0 +1,17 @@
+//
+//  ValidationVM.swift
+//  Pods
+//
+//  Created by Brandon Gouws on 2020/02/26.
+//
+
+import Foundation
+
+class PTValidation {
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+}
