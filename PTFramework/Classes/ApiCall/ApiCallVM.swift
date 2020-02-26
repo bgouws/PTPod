4bb455076e4865b8cfd3a8c0c7b9f559c89ae160
Brandon Gouws <bgouws@jhb.dvt.co.za>
2020-02-26 09:16:19 +0200

    Added more file structure

---
+++ PTFramework/Classes/ApiCall/ApiCallVM.swift
@@ -0,0 +1,14 @@
+//
+//  ApiCallVM.swift
+//  PTFramework
+//
+//  Created by Brandon Gouws on 2020/02/26.
+//
+
+import Foundation
+
+public class PTApiCall {
+    public static func ptCallApi() {
+        
+    }
+}
---
+++ PTFramework/Classes/TasteSelection/TasteSelectionVM.swift
@@ -0,0 +1,14 @@
+//
+//  TasteSelectionVM.swift
+//  PTFramework
+//
+//  Created by Brandon Gouws on 2020/02/26.
+//
+
+import Foundation
+
+public class PTTasteSelection {
+    public static func ptTasteSelection(taste: [String]) {
+        
+    }
+}
