8269e7bf0350403679d558d0e8b4c1e5b53b546d
Brandon Gouws <bgouws@jhb.dvt.co.za>
2020-04-05 14:25:03 +0200

    Prep for api call

---
+++ PTAPI Call/ptAPICallModel.swift
@@ -0,0 +1,15 @@
+//
+//  ptAPICallModel.swift
+//  PTFramework
+//
+//  Created by Brandon Gouws on 2020/04/05.
+//
+
+import Foundation
+
+struct Quote {
+    var id: Int?
+    var firstName: String
+    var lastName: String
+    var quote: String
+}
